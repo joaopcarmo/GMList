58576148bc5c9dd172cd2742e257eafcebb17b6f
João Pedro <joaocarmolp@gmail.com>
2025-05-19 14:40:14 -0300

    Project created

---
+++ src/test/java/com/intensivao/gmlist/GmlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intensivao.gmlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
