d9fd43b39665394fd475cfb93bb1a85cda66f6b7
João Pedro <joaocarmolp@gmail.com>
2025-05-20 15:09:26 -0300

    feat: add endpoint to get game details by ID

--- src/main/java/com/intensivao/gmlist/controllers/GameController.java
+++ src/main/java/com/intensivao/gmlist/controllers/GameController.java
@@ -1,11 +1,13 @@
 package com.intensivao.gmlist.controllers;
 
 
+import com.intensivao.gmlist.dto.GameDto;
 import com.intensivao.gmlist.dto.GameMinDto;
 import com.intensivao.gmlist.entities.Game;
 import com.intensivao.gmlist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,12 @@ public class GameController {
     @GetMapping
     public List<GameMinDto> findAll(){
         List<GameMinDto> result = gameService.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDto findById(@PathVariable Long id){
+        GameDto result = gameService.findById(id);
         return result;
     }
 }
--- src/main/java/com/intensivao/gmlist/services/GameService.java
+++ src/main/java/com/intensivao/gmlist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.intensivao.gmlist.services;
 
+import com.intensivao.gmlist.dto.GameDto;
 import com.intensivao.gmlist.dto.GameMinDto;
 import com.intensivao.gmlist.entities.Game;
 import com.intensivao.gmlist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,11 +17,19 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDto findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDto(result);
+    }
+
+
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll() {
         List<Game> result = gameRepository.findAll();
         // Converte cada Game para GameMinDto usando stream e mapeamento
-        List<GameMinDto> dto = result.stream().map(x -> new GameMinDto(x)).toList();
+        return result.stream().map(x -> new GameMinDto(x)).toList();
         // Retorna a lista de DTOs para ser usada em camadas superiores (ex: controller)
-        return dto;
+
     }
 }
